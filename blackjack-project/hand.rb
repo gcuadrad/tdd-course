abc03e13b2cd6acfc7e9eb4af8e6c0869e6b88a6
Gonzalo Cuadra <gonzalocuadra@protonmail.com>
2019-10-12 22:44:33 -0300

    Components Part 2

--- blackjack-project/hand.rb
+++ blackjack-project/hand.rb
@@ -1,6 +1,4 @@
 class Hand
-<<<<<<< HEAD
-=======
   VALUES = { '2': 2,
              '3': 3,
              '4': 4,
@@ -14,7 +12,6 @@ class Hand
              'Queen': 10,
              'King': 10,
              'Ace': 1 }
->>>>>>> f5502e45d2f2192d9223e0552001859e4719a95a
   attr_accessor :dealt_card
   def initialize
     @dealt_card = []
@@ -26,15 +23,6 @@ class Hand
 
   def get_value
     # get rank of cards and put in an array
-<<<<<<< HEAD
-    card_ranks = []
-    dealt_card.each { |card| card_ranks.push(card.rank) }
-
-    value = 0
-    card_ranks.each do |rank|
-
-    end
-=======
     # card_ranks = []
     # dealt_card.each { |card| card_ranks.push(card.rank) }
     card_ranks = dealt_card.map { |card| card.rank }
@@ -46,6 +34,5 @@ class Hand
     value = card_ranks.reduce(0) { |acc, rank| acc + VALUES[rank.to_sym] }
     value += 10 if card_ranks.include?('Ace') && value + 10 <= 21
     value
->>>>>>> f5502e45d2f2192d9223e0552001859e4719a95a
   end
 end
